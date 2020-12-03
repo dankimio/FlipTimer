2f8cd8fc89946954cede671492af78f0ef9ec134
Dan Kim <itsdanya@gmail.com>
2020-12-03 16:12:12 +0300

    Initial Commit

---
+++ FlipTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlipTimer
+//
+//  Created by Dan on 03/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlipTimer/FlipTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipTimerApp.swift
+//  FlipTimer
+//
+//  Created by Dan on 03/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FlipTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
