ccf5732591f1552ee1a8f38625e4d62297ae7575
Dan Kim <itsdanya@gmail.com>
2020-12-03 17:09:08 +0300

    Add navigation link

--- FlipTimer/ContentView.swift
+++ FlipTimer/ContentView.swift
@@ -1,27 +1,31 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    NavigationView {
-      VStack {
-        Text("24:59")
-          .font(.system(size: 96, weight: .light, design: .monospaced))
-          .padding(.bottom, 30)
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("24:59")
+                    .font(.system(size: 96, weight: .light, design: .monospaced))
+                    .padding(.bottom, 30)
 
-        VStack {
-          Text("Flip your phone to start the timer")
-        }
-      }.navigationBarItems(trailing: Button(action: {
-        print("Settings")
-      }) {
-        Image(systemName: "gearshape.fill")
-      })
+                VStack {
+                    Text("Flip your phone to start the timer")
+                }
+            }
+            .navigationBarItems(
+                trailing: NavigationLink(
+                    destination: Text("Destination"),
+                    label: {
+                        Image(systemName: "gearshape.fill")
+                    }
+                )
+            )
     }.accentColor(.black)
-  }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
