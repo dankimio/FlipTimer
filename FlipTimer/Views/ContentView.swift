2b1f5063b7a8bf9f30669cbd4df679513cd5a58f
Dan Kim <itsdanya@gmail.com>
2020-12-04 22:10:31 +0300

    Add Stop button

--- FlipTimer/Views/ContentView.swift
+++ FlipTimer/Views/ContentView.swift
@@ -2,21 +2,38 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showSettingsView = false
-
+    @State var timerMode: TimerMode
+    
     var body: some View {
         NavigationView {
             VStack {
                 Text("24:59")
                     .font(.system(size: 96, weight: .regular, design: .monospaced))
                     .padding(.bottom, 30)
-
+                
                 VStack {
-                    Text("Flip your phone to start the timer")
-                        .foregroundColor(Color(UIColor.systemGray))
+                    if timerMode == .initial {
+                        Text("Flip your phone to start the timer")
+                            .foregroundColor(Color(UIColor.systemGray))
+                    }
+
+                    if timerMode == .paused {
+                        Button(action: {}, label: {
+                            HStack {
+                                Spacer()
+                                Text("Stop").foregroundColor(.white)
+                                Spacer()
+                            }
+                            .padding()
+                            .background(Color.black)
+                            .cornerRadius(12)
+                        })
+                    }
                 }
 
                 Spacer().frame(maxHeight: 200)
             }
+            .padding()
             .navigationBarItems(
                 trailing: Button(
                     action: { showSettingsView.toggle() },
@@ -32,6 +49,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(timerMode: .paused)
     }
 }
