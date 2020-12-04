0160ed2cb43864acc99c8cb13b546716b95a7815
Dan Kim <itsdanya@gmail.com>
2020-12-05 02:21:21 +0300

    Extract proximitySensorDidClose and proximitySensorDidUnclose

--- FlipTimer/View Models/TimerManager.swift
+++ FlipTimer/View Models/TimerManager.swift
@@ -8,7 +8,7 @@ enum TimerMode {
 }
 
 class TimerManager: ObservableObject {
-    var timerMode: TimerMode = .initial
+    @Published var timerMode: TimerMode = .initial
 
     // Source: https://gist.github.com/skl/a093291abc0a90a640e50f78888456e7
     @objc func proximityDidChange(notification: NSNotification) {
@@ -16,8 +16,16 @@ class TimerManager: ObservableObject {
 
         guard let device = notification.object as? UIDevice else { return }
 
-        // BeginRecording, EndRecording
-        let systemSound = device.proximityState ? SystemSoundID(1117) : SystemSoundID(1118)
-        AudioServicesPlayAlertSound(systemSound)
+        device.proximityState ? proximitySensorDidClose() : proximitySensorDidUnclose()
+    }
+
+    private func proximitySensorDidClose() {
+        // BeginRecording
+        AudioServicesPlayAlertSound(SystemSoundID(1117))
+    }
+
+    private func proximitySensorDidUnclose() {
+        // EndRecording
+        AudioServicesPlayAlertSound(SystemSoundID(1118))
     }
 }
