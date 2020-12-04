2b0d35a077c52d3e3a4bcc804ccf0315ac7bad03
Dan Kim <itsdanya@gmail.com>
2020-12-04 23:17:57 +0300

    Add ProximityObserver

--- FlipTimer/Views/ContentView.swift
+++ FlipTimer/Views/ContentView.swift
@@ -1,8 +1,20 @@
 import SwiftUI
 
+// Source: https://gist.github.com/skl/a093291abc0a90a640e50f78888456e7
+class ProximityObserver {
+    @objc func didChange(notification: NSNotification) {
+        print("ProximityObserver.didChange")
+        if let device = notification.object as? UIDevice {
+            print(device.proximityState)
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var showSettingsView = false
     @State var timerMode: TimerMode = .paused
+
+    var proximityObserver = ProximityObserver()
     
     var body: some View {
         NavigationView {
@@ -44,6 +56,40 @@ struct ContentView: View {
         .sheet(isPresented: $showSettingsView, content: {
             SettingsView(showSettingsView: $showSettingsView)
         })
+        .onAppear(perform: {
+            print("onAppear")
+
+            activateProximitySensor()
+        })
+        .onDisappear(perform: {
+            print("onDisappear")
+
+            deactivateProximitySensor()
+        })
+    }
+
+    func activateProximitySensor() {
+        print("activateProximitySensor")
+        UIDevice.current.isProximityMonitoringEnabled = true
+
+        if UIDevice.current.isProximityMonitoringEnabled {
+            NotificationCenter.default.addObserver(
+                proximityObserver,
+                selector: #selector(proximityObserver.didChange),
+                name: UIDevice.proximityStateDidChangeNotification,
+                object: UIDevice.current
+            )
+        }
+    }
+
+    func deactivateProximitySensor() {
+        print("deactivateProximitySensor")
+        UIDevice.current.isProximityMonitoringEnabled = false
+        NotificationCenter.default.removeObserver(
+            proximityObserver,
+            name: UIDevice.proximityStateDidChangeNotification,
+            object: UIDevice.current
+        )
     }
 }
 
