4689150a0a0e74cef8f18c682bdc197c6d04d096
Dan Kim <itsdanya@gmail.com>
2020-12-07 22:42:21 +0300

    Extract startMotionManager

--- FlipTimer/View Models/MotionManager.swift
+++ FlipTimer/View Models/MotionManager.swift
@@ -3,14 +3,18 @@ import CoreMotion
 final class MotionManager: ObservableObject {
     private var motionManager = CMMotionManager()
 
+    @Published var isDeviceUpsideDown: Bool?
+
     private var x = 0.0
     private var y = 0.0
     private var z = 0.0
 
     init() {
-        guard motionManager.isAccelerometerAvailable else {
-            return
-        }
+        startMotionManager()
+    }
+
+    private func startMotionManager() {
+        guard motionManager.isAccelerometerAvailable else { return }
 
         motionManager.accelerometerUpdateInterval = 1.0 / 20.0
         motionManager.startAccelerometerUpdates(to: .main) { (accelerometerData, error) in
