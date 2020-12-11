7c0354b60e8f85a64a635153cd9f09ac8ffcd6b0
Dan Kim <itsdanya@gmail.com>
2020-12-12 00:08:05 +0300

    Add FeatureCell

---
+++ FlipTimer/Views/FeatureCell.swift
@@ -0,0 +1,45 @@
+//
+//  FeatureCell.swift
+//  FlipTimer
+//
+//  Created by Dan on 12/12/2020.
+//
+
+import SwiftUI
+
+struct FeatureCell: View {
+    var imageName: String
+    var title: String
+    var subtitle: String
+
+    var body: some View {
+        HStack(spacing: 24) {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 32)
+                .foregroundColor(.blue)
+
+            VStack(alignment: .leading, content: {
+                Text(title)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                Text(subtitle)
+                    .foregroundColor(.secondary)
+                    .font(.subheadline)
+            })
+
+            Spacer()
+        }
+    }
+}
+
+struct FeatureCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCell(
+            imageName: "text.badge.checkmark",
+            title: "Title",
+            subtitle: "Subtitle"
+        )
+    }
+}
