6057b446d88f48291d43de4a26744220ca0da572
Dan Kim <itsdanya@gmail.com>
2023-10-21 20:07:36 +0100

    singleton class

--- FlipTimer/Utilities/Flashlight.swift
+++ FlipTimer/Utilities/Flashlight.swift
@@ -1,1 +1,13 @@
+import AVFoundation
 
+struct Flashlight {
+    static let shared = Flashlight()
+
+    private let device = AVCaptureDevice.default(for: .video)
+
+    private init() {}
+
+    var hasFlashlight: Bool {
+        return device?.hasTorch ?? false
+    }
+}
