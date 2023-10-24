e3f76b792c2c7d48c2519d71b926dca8986346c1
Dan Kim <itsdanya@gmail.com>
2023-10-24 19:13:23 +0100

    update manager

--- FlipTimer/Utilities/IconManager.swift
+++ FlipTimer/Utilities/IconManager.swift
@@ -1,15 +1,30 @@
 import Foundation
 import UIKit
 
+// Source: https://developer.apple.com/documentation/xcode/configuring_your_app_to_use_alternate_app_icons
+enum Icon: String, CaseIterable, Identifiable {
+    case primary = "AppIcon"
+    case classicDark = "AppIconClassicDark"
+    case classicLight = "AppIconClassicLight"
+
+    var id: String {
+        self.rawValue
+    }
+}
+
+// Source: https://developer.apple.com/documentation/xcode/configuring_your_app_to_use_alternate_app_icons
 class IconManager {
-    @Published var icon = "AppIcon"
+    @Published var icon: Icon = .primary
 
     /// Change the app icon
-    func setAlternateAppIcon(icon: String) {
-        // Avoid setting the name if the app already uses that icon.
-        guard UIApplication.shared.alternateIconName != icon else { return }
+    func setAlternateAppIcon(icon: Icon) {
+        // Set the icon name to nil to use the primary icon.
+        let iconName: String? = (icon != .primary) ? icon.rawValue : nil
 
-        UIApplication.shared.setAlternateIconName(icon) { (error) in
+        // Avoid setting the name if the app already uses that icon.
+        guard UIApplication.shared.alternateIconName != iconName else { return }
+
+        UIApplication.shared.setAlternateIconName(iconName) { (error) in
             if let error = error {
                 print("Failed request to update the app’s icon: \(error)")
             }
@@ -23,6 +38,6 @@ class IconManager {
             return
         }
 
-        icon = alternateIconName
+        icon = Icon(rawValue: alternateIconName)!
     }
 }
